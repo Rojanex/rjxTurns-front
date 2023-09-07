51f759ec16920cdb31299e22aec16ad540dd5b9f
Lu <77848575+Rojanex@users.noreply.github.com>
2023-09-07 12:45:59 -0500

    add steps in ingrese

--- src/layouts/toma-tu-turno/IngreseNombre.js
+++ src/layouts/toma-tu-turno/IngreseNombre.js
@@ -1,117 +1,98 @@
-// react-router-dom components
-import { Link } from "react-router-dom";
-
-// @mui material components
-import Card from "@mui/material/Card";
-import Checkbox from "@mui/material/Checkbox";
-
-// Material Dashboard 2 React components
-import MDBox from "components/MDBox";
-import MDTypography from "components/MDTypography";
-import {
-  Grid,
-  Table,
-  TableRow,
-  TableCell,
-  Box,
-  Button,
-  TextField,
-} from "@mui/material";
-import CardMedia from "@mui/material/CardMedia";
-
-// Authentication layout components
 import CoverLayoutMonitor from "layouts/monitor/components/CoverLayoutMonitor";
 import axios from "axios";
-import io from "socket.io-client";
-import Alarm from "assets/sound/evacuation-alert.mp3";
-import { useState, useEffect } from "react";
+import { useState } from "react";
+import { TextField, Checkbox, Button, Box, Card } from "@mui/material";
+import MDBox from "components/MDBox";
+import MDTypography from "components/MDTypography";
 
-// Images
+const NameStep = ({ name, setName, setStep }) => (
+  
+  <Card sx={{ width: "fit-content", margin: "3rem", width: "450px", padding: "1rem" }}>
+    <MDTypography  sx={{ mt: 5, mb: 5,  textAlign: "center", color: "black" }} variant="h3" fontWeight="bold" color="black">
+      Ingrese su nombre
+    </MDTypography>
+    <TextField
+      label="Nombre"
+      value={name}
+      onChange={(e) => setName(e.target.value)}
+      InputProps={{
+        style: { fontSize: "2rem" },
+      }}
+    />
+    <Button
+      onClick={() => setStep(2)}
+      variant="contained" 
+      sx={{ mt: 5, mb: 1, fontSize: "1.2rem"}}
+    >
+      <MDTypography variant="text" fontWeight="small" color="orange" /> Siguiente
+    </Button>
+  </Card>
+);
 
-function IngreseNombre({ transparent, light, action }) {
-  const [filteredItems, setFilteredItems] = useState([]);
-  const [highlight, setHighlight] = useState(false);
-  const globalIP = process.env.REACT_APP_GLOBAL_IP;
+const PriorityStep = ({ priority, setPriority, setStep }) => (
+  
+  <Card sx={{ width: "fit-content", margin: "3rem", width: "450px", padding: "1rem" }}>
+    <MDTypography  sx={{ mt: 5, mb: 1,  textAlign: "center", color: "black" }} variant="h3" fontWeight="bold" color="black">Tipo de Atencion
+    </MDTypography>
+    <MDBox sx={{ display: "flex", justifyContent: "center" }}>
+    <Button
+    variant="contained" 
+    sx={{ mt: 3, mb: 1, mr: 3, fontSize: "1.2rem", color:"black"} }
+    onClick={() => setPriority(0)}>
+      <MDTypography variant="" fontWeight="" sx={{color:"black"}} />Atencion Prioritaria</Button>
+    </MDBox>
+    <MDBox sx={{ display: "flex", justifyContent: "center" }}>
+    <Button
+    variant="contained" 
+    sx={{ mt: 5, mb: 1, mr: 3, fontSize: "1.2rem", color:"black"} }
+    onChange={() => setPriority(1)}>
+      <MDTypography variant="" fontWeight="" sx={{color:"black"}} />Atencion General</Button>
+    </MDBox>
+    <MDBox sx={{ display: "flex", justifyContent: "center" }}>
+    <Button
+    variant="" 
+    sx={{ mt: 5, mb: 1, mr: 3, fontSize: "1.2rem", color:"black"} }
+    onClick={() => setStep(1)}>
+      <MDTypography variant="" fontWeight="" sx={{color:"black"}} />Atras</Button>
+    <Button 
+    variant="contained" 
+    sx={{ mt: 5, mb: 1, fontSize: "1.2rem", color:"orange"} }
+    onClick={() => setStep(3)}>
+      <MDTypography variant="text" fontWeight="small" color="orange" />
+      Siguiente</Button>
+    </MDBox>
+  </Card>
 
-  useEffect(() => {
-    const socket = io(`http://${globalIP}`, { transports: ["polling"] });
-    socket.on("connect", () => {
-      console.log("Socket connected");
-    });
+);
 
-    axios
-      .get(`http://${globalIP}/data/attend_elements`)
-      .then((response) => {
-        const data = response.data.data;
-        setFilteredItems(data);
-        console.log(typeof data);
-      })
-      .catch((error) => {
-        console.error("Error fetching data from API:", error);
-      });
+const ReviewStep = ({ name, priority }) => (
+  <div>
+    <p>Nombre: {name}</p>
+    <p>Prioridad: {priority ? "Si" : "No"}</p>
+    <Button>Confirmar</Button>
+  </div>
+);
 
-    socket.on("element_removed", function (msg) {
-      // Update your table in the UI here
-      setHighlight(true);
-
-      // Change row color back to white after 10 seconds
-      setTimeout(() => {
-        setHighlight(false);
-      }, 10000);
-
-      console.log("received message: " + msg.message);
-      console.log("removed element: ", msg.removedElement);
-      var audio = new Audio(Alarm);
-      audio.play();
-      audio.loop = true;
-      // Stop the audio after 10 seconds
-      setTimeout(function () {
-        audio.pause();
-        audio.currentTime = 0;
-      }, 10000);
-      axios
-        .get(`http://${globalIP}/data/attend_elements`)
-        .then((response) => {
-          const data = response.data.data;
-          setFilteredItems(data);
-          console.log(typeof data);
-        })
-        .catch((error) => {
-          console.error("Error fetching data from API:", error);
-        });
-    });
-
-    return () => {
-      // Clean up the socket connection when the component unmounts
-      socket.disconnect();
-    };
-  }, []);
+function IngreseNombre() {
+  const [step, setStep] = useState(1);
+  const [name, setName] = useState("");
+  const [priority, setPriority] = useState(1);
 
   return (
-    <CoverLayoutMonitor
-      sx={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Box xs={{ display: "flex", flexDirection: "row" }}>
-        <TextField
-          variant="outlined"
-          size="large"
-          sx={{
-            mt: 1,
-            mb: 1,
-            backgroundColor: "#FFFFFF", // white background color
-            color: "#000000", // black text color
-            // &:hover: {
-            //   backgroundColor: "#0069d9", // removed hover effect
-            // },
-            padding: "20px 50px",
-            fontSize: "50px",
-          }}
-        />
-      </Box>
+    <CoverLayoutMonitor>
+      <div>
+        {step === 1 && (
+          <NameStep name={name} setName={setName} setStep={setStep} />
+        )}
+        {step === 2 && (
+          <PriorityStep
+            priority={priority}
+            setPriority={setPriority}
+            setStep={setStep}
+          />
+        )}
+        {step === 3 && <ReviewStep name={name} priority={priority} />}
+      </div>
     </CoverLayoutMonitor>
   );
 }
